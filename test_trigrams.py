20a65d3f10c742fa8c164dc65e2286bdf89dea99
Kurt <kurtrm@gmail.com>
2017-05-17 15:27:53 -0700

    Finished function and test, not tested.

--- test_trigrams.py
+++ test_trigrams.py
@@ -1,6 +1,9 @@
+import pytest
+
+
 
 def test_trigrams():
     """test trigrams function"""
     from trigrams import main
-    assert main(text.txt, 6) == {"seattle weather": "is", "weather is":"crazy,", "is crazy,": "I", "crazy, I": "need"}
-
+    assert main('text.txt', 6) == {"seattle weather": "is",
+    "weather is": "crazy,", "is crazy,": "I", "crazy, I": "need"}
