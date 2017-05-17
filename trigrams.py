c949ae44fe36ccb143923b026b3330107cbc9ff2
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-17 15:58:36 -0700

    add test files

--- trigrams.py
+++ trigrams.py
@@ -1,3 +1,10 @@
 def main(file, x):
     read_file  = open(file).read()
 
+
+  output = []
+  for i in range(len(input)-n+1):
+    output.append(input[i:i+n])
+  return output
+
+[' '.join(x) for x in ngrams('a b c d', 2)]
