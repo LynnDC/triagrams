e1c5cb0aca6b2328112f70551dcddb2a0fe35f0e
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-18 14:43:51 -0700

    initial working test, needs to do more testing

--- src/test_trigrams.py
+++ src/test_trigrams.py
@@ -1,9 +1,14 @@
+"Tests for trigrams funcitons"
 import pytest
 
 
-def test_trigrams():
-    """test trigrams function"""
-    from trigrams import main
-    is_dict = main('text.txt', 4)
-    assert isinstance(is_dict, dict)
-    assert len(is_dict) == 17
+def test_parse_words():
+    """test if our function will return a dictionary with 2 word key and a list or string value"""
+    from trigrams import parse_words
+    my_words = ["seattle", "weather", "is", "crazy", "I", "am", "getting", "fat", "and", "I", "am", "so", "sleepy", "oh", "my", "god"]
+    # result = {"seattle weather": "is", "weather is": "crazy", "is crazy": "I", "I am": ["getting", "so"], "am getting": "fat", "fat and": "I", "so sleep": "oh", "oh my": "god"}
+    a = (' ').join(my_words[: 2])
+    b = my_words[2]
+    c = parse_words(my_words)
+    assert c[a] == b
+
