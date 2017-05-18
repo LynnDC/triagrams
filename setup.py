66071b177185b559ba875a1e4a4ea164bffebbdf
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-18 01:45:59 -0700

    finished setup.py with distribution tracker, dependencies and entry points

--- setup.py
+++ setup.py
@@ -9,5 +9,11 @@ setup(
     author_email="",
     license='MIT',
     py_modules=['trigrams'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    extras_require={'test': ['pytest', 'pytest-watch']},
+    entry_points={
+        'console_scripts': [
+            "trigrams = trigrams:main"
+        ]
+    }
 )
