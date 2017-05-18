96e622c91f85168b065d3339df3572675c08c20f
Kurt <kurtrm@gmail.com>
2017-05-17 19:07:33 -0700

    Got some logic working on the main function.

--- trigrams.py
+++ trigrams.py
@@ -6,12 +6,24 @@ import io
 def main(file, x):
     """Opens file, splits it, grabs the desired chunk
         and makes a dictionary out of the words"""
+
+    # This is going to be a separate function
     with io.open(file, encoding='utf=8') as test:
         text = test.read().split()
     # Logic goes here to remove undesired characters
     text_dict = {}
-    for i in range(len(text) - x + 2):
+    for i in range(len(text) - 2):
         two_words = (' ').join(text[i: i + 2])
         third_word = text[i + 2]
-        text_dict.setdefault(two_words, third_word)
+        if two_words in text_dict.keys():
+            if isinstance(text_dict[two_words], list):
+                text_dict[two_words].append(third_word)
+            else:
+                text_dict[two_words] = list(text_dict[two_words])
+                text_dict[two_words].append(third_word)
+        else:
+            text_dict.setdefault(two_words, third_word)
     return text_dict
+
+    # main() will actually return our desired paragraph
+    # eventually
