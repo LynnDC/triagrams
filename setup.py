cc7970645d90163ccdd9278003371cb4770b9d1c
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-18 01:25:45 -0700

    create distribution package in setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""creating python distribution package setup.py"""
+from setuptools import setup
+
+setup(
+    name="trigrams",
+    description="A Python implementation of a trigrams Function.",
+    version=0.1,
+    author="Lynn Chen & Kurt Maurer",
+    author_email="",
+    license='MIT',
+    py_modules=['trigrams'],
+    package_dir={'': 'src'}
+)
